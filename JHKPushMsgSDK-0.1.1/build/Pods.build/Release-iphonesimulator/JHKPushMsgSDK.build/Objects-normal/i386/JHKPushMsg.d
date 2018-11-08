89d93524928299b52f3008d56e682c883ef1330a
mazhendong <mazhendong@hisense.com>
2018-11-08 10:49:16 +0800

    0.1.2

--- JHKPushMsgSDK-0.1.1/build/Pods.build/Release-iphonesimulator/JHKPushMsgSDK.build/Objects-normal/i386/JHKPushMsg.d
+++ JHKPushMsgSDK-0.1.1/build/Pods.build/Release-iphonesimulator/JHKPushMsgSDK.build/Objects-normal/i386/JHKPushMsg.d
@@ -1,8 +1,8 @@
 dependencies: \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/JHKPushMsgSDK/JHKPushMsgSDK/Classes/JHKPushMsg.m \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Target\ Support\ Files/JHKPushMsgSDK/JHKPushMsgSDK-prefix.pch \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/JHKPushMsgSDK/JHKPushMsgSDK/Classes/JHKPushMsg.h \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CloudPushSDK.h \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CCPSysMessage.h \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Headers/Public/AlicloudPush/CloudPushSDK/MPGerneralDefinition.h \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CloudPushCallbackResult.h
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/JHKPushMsgSDK/JHKPushMsgSDK/Classes/JHKPushMsg.m \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Target\ Support\ Files/JHKPushMsgSDK/JHKPushMsgSDK-prefix.pch \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/JHKPushMsgSDK/JHKPushMsgSDK/Classes/JHKPushMsg.h \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CloudPushSDK.h \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CCPSysMessage.h \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Headers/Public/AlicloudPush/CloudPushSDK/MPGerneralDefinition.h \
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Headers/Public/AlicloudPush/CloudPushSDK/CloudPushCallbackResult.h
--- JHKPushMsgSDK-0.1.1/build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
+++ JHKPushMsgSDK-0.1.1/build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-ob4bs1g4/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m
+  /var/folders/gl/s1xdn4_17t55y2vsd0k_y5nw0000gn/T/cocoapods-3qlqya6b/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m
